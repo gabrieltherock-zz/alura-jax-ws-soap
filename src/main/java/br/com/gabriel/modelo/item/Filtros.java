4a4b3be0c2877dc3f9bb91bcb715da96d3034cd8
gabrieltherock <gabrieldarocha2013@hotmail.com>
2021-07-18 16:29:14 -0300

    commit inicial

---
+++ src/main/java/br/com/gabriel/modelo/item/Filtros.java
@@ -0,0 +1,20 @@
+package br.com.gabriel.modelo.item;
+
+import java.util.List;
+
+public class Filtros {
+
+	private List<Filtro> filtros;
+
+	public Filtros(List<Filtro> filtros) {
+		this.filtros = filtros;
+	}
+
+	Filtros() {
+	}
+	
+	public List<Filtro> getLista() {
+		return filtros;
+	}
+	
+}
